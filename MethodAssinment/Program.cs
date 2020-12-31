724ccdba1d42862ba2870e3a06c33be3ffaa44e5
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-12-31 16:28:29 -0700

    Add project files.

---
+++ MethodAssinment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MethodAssinment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            newMeths math = new newMeths();
+            math.addMeth(num1);
+            Console.WriteLine(math.addMeth(num1));
+            math.decMeth(num1);
+            Console.WriteLine(math.decMeth(num1));
+            string num2 = "10";
+            math.writtenMeth(num2);
+            Console.WriteLine(math.writtenMeth(num2));
+
+        }
+    }
+}
---
+++ MethodAssinment/newMeths.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodAssinment
+{
+    public class newMeths
+    {
+        public int addMeth(int num1)
+        {
+            int result;
+            result = num1 + 10;
+            return result;
+        }
+        public double decMeth(int num1)
+        {
+            double result;
+            result = num1 + 4.33365453;
+            return result;
+        }
+        public int writtenMeth(string num2)
+        {
+            int result;
+            int newResult;
+            result = Convert.ToInt32(num2);
+            newResult = result + 22;
+            return newResult;
+        }
+
+    }
+}
